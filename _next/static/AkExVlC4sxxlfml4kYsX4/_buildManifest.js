5f3d15662100083289a6b64940adda71978612b4
YoungKind111 <YoungKind111@users.noreply.github.com>
2026-02-22 23:11:01 +0800

    Fix image paths for GitHub Pages

---
+++ _next/static/AkExVlC4sxxlfml4kYsX4/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/chaowan//_next/:path+",
+        "destination": "/chaowan/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
